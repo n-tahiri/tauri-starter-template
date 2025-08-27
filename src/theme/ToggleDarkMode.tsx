a8f9a403fbacb4d0c2614f56bc52012754c3da23
nTahiri <nt-dev@outlook.com>
2025-08-27 22:09:57 +0200

    Fixed dark mode toggle icon

--- src/theme/ToggleDarkMode.tsx
+++ src/theme/ToggleDarkMode.tsx
@@ -1,7 +1,5 @@
-import { Moon, Sun } from "lucide-react";
-
-// import { Button } from "@/components/ui/button";
 import { useTheme } from "./theme-provider";
+import { Moon, Sun } from "lucide-react";
 import { Button } from "@heroui/react";
 
 export function ToggleDarkMode() {
@@ -12,7 +10,12 @@ export function ToggleDarkMode() {
   };
 
   return (
-    <Button onPress={toggleTheme} aria-label="Toggle theme" className="w-7 h-7">
+    <Button
+      onPress={toggleTheme}
+      aria-label="Toggle theme"
+      variant="light"
+      isIconOnly
+    >
       <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
     </Button>
