ee8828056baffdf9a77ad71764d52f3eb9000938
nTahiri <nt-dev@outlook.com>
2025-08-27 22:16:22 +0200

    Added missing ThemeProvider in App.tsx for Dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import HeaderContent from "./components/1_HeaderContent";
 import "./App.css";
 import { DockDemo } from "./demos/DockDemo";
 import NextButton from "./demos/NextButton";
+import { ThemeProvider } from "./theme/theme-provider";
 
 export default function App() {
   return (
-    <main className="flex flex-col gap-4">
-      <HeaderContent />
-      <NextButton />
-      {/* <DockDemo /> */}
-    </main>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="flex flex-col gap-4">
+        <HeaderContent />
+        <NextButton />
+        {/* <DockDemo /> */}
+      </main>
+    </ThemeProvider>
   );
 }
