5f0f756084c230dd5e216d7e67c145b6ca0bccfb
nTahiri <nt-dev@outlook.com>
2025-08-27 21:57:51 +0200

    Configured Tauri & Fixed early load of white window => Show window only if Frontend is ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+
+/* SET WINDOW VISIBLE WHEN READY */
+document.addEventListener("DOMContentLoaded", async () => {
+  // console.log("DOMContentLoaded fired");
+  await getCurrentWindow().show();
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
