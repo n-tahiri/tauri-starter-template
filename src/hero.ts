2b0d9987619d5b5151a02f210233e4a14616e217
nTahiri <nt-dev@outlook.com>
2025-08-27 22:09:38 +0200

    Adjusted dark mode

--- src/hero.ts
+++ src/hero.ts
@@ -1,11 +1,13 @@
 import { heroui } from "@heroui/react";
 export default heroui({
   themes: {
+    /* Custom Dark Theme, it is already implemented in App.css */
     dark: {
       extend: "dark",
       colors: {
-        background: "#19181aff",
-        foreground: "#ffffff",
+        background: "#191818",
+        // background: "#19181aff",
+        // foreground: "#ffffff",
       },
     },
   },
