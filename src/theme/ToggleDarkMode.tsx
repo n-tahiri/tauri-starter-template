68e35ac0c86f952772a1ae5e5081e3a6d369fc76
nTahiri <nt-dev@outlook.com>
2025-08-27 21:59:27 +0200

    Added Dark Mode support

---
+++ src/theme/ToggleDarkMode.tsx
@@ -0,0 +1,20 @@
+import { Moon, Sun } from "lucide-react";
+
+// import { Button } from "@/components/ui/button";
+import { useTheme } from "./theme-provider";
+import { Button } from "@heroui/react";
+
+export function ToggleDarkMode() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button onPress={toggleTheme} aria-label="Toggle theme" className="w-7 h-7">
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+    </Button>
+  );
+}
